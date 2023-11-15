17098aafc309875d74d70d9e0aa9579e99de2aa0
Develcode119 <develcode119@Develcode119s-MacBook-Pro.local>
2023-11-15 15:37:15 -0400

    feat: cors habilitado

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 import express from "express";
 import db from "./config/dbConect.js";
 import routes from "./routes/index.js"
+import cors from 'cors'
 
 
 db.on("error", console.log.bind(console, "Conexão com o banco falhou!"));
@@ -12,6 +13,8 @@ db.once("open", () => {
 
 //instanciando o express
 const app = express()
+
+app.use(cors(['http://localhost:3001']))
 
 //habilitando o uso de json pelo express
 app.use(express.json())
